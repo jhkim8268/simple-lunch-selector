2bf73667fe54a68137ead383b37278d3757a2566
jhkim8268@gmail.com <jhkim8268@gmail.com>
2019-09-04 11:09:35 -0700

    project work locally

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./server');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
