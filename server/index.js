315f45ab144e58b388125a030576e7182f4a4c47
jinkim-tech <jhkim8268@gmail.com>
2019-11-07 19:23:50 -0800

    changed port to fix code H10 error in Heroku

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./server');
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
